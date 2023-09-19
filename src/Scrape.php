e78568bef64a65d03483c2bae099cc4b1fec3462
Frederick Adebayo <sprintcorp7@gmail.com>
2023-09-19 11:42:11 +0100

    logic to fetch document within scrape file

--- src/Scrape.php
+++ src/Scrape.php
@@ -9,57 +9,53 @@ require 'vendor/autoload.php';
 class Scrape
 {
     private array $products = [];
-
-    const  WEBSITE_URL = 'https://www.magpiehq.com/developer-challenge/smartphones';
-
+    private const WEBSITE_URL = 'https://www.magpiehq.com/developer-challenge/smartphones';
 
     public function run(): void
     {
-    
+        // Fetch the initial document
         $document = ScrapeHelper::fetchDocument(self::WEBSITE_URL);
 
-        //Get the number of pages
-        $numOfPages = $this->totalPages($document);
+        // Get the number of pages
+        $numOfPages = $this->getNumOfPages($document);
 
-        // Initialize an empty associative array to store unique product options
-        $uniqueProductOptions = [];
-
+        // Iterate through the pages of the website
         for ($i = 1; $i <= $numOfPages; $i++) {
+            // Fetch the document for the current page
             $pageUrl = self::WEBSITE_URL . '/?page=' . $i;
-            $document = ScrapeHelper::fetchDocument($pageUrl);
-            $items = $document->filter(".product");
+            $pageDocument = ScrapeHelper::fetchDocument($pageUrl);
 
+            // Filter products on the current page
+            $items = $pageDocument->filter('div.product.px-4');
+
+            // For each product, get its details
             foreach ($items as $element) {
                 $elementCrawler = new Crawler($element, $pageUrl);
                 $productOptions = Product::getProduct($elementCrawler);
 
-                // Merge the product options into the uniqueProductOptions array
+                // Check each color variant and add it to the products array if not present
                 foreach ($productOptions as $colorOption) {
-                    $uniqueProductOptions[$colorOption['title']][$colorOption['colour']] = $colorOption;
+                    if (!in_array($colorOption, $this->products)) {
+                        $this->products[] = $colorOption;
+                    }
                 }
             }
         }
 
-        // Convert the associative array of unique product options back to a flat array
-        $this->products = array_merge(...array_values($uniqueProductOptions));
-
-        // Reset array keys to ensure a sequential array
-        $this->products = array_values($this->products);
-
-
-        file_put_contents('output.json', json_encode($this->products, JSON_PRETTY_PRINT ));
+        file_put_contents('output.json', json_encode($this->products, JSON_PRETTY_PRINT));
     }
 
-    
-    private function totalPages($document) {
+    // Method returns the total number of pages
+    private function getNumOfPages($document)
+    {
         $value = $document->filter('#products')->text();
         preg_match_all('!\d+!', $value, $matches);
-        $numOfPages = (int) $matches[0][1];
+
+        $numOfPages = (int)$matches[0][1];
+
         return $numOfPages;
     }
-
 }
-
 
 $scrape = new Scrape();
 $scrape->run();
